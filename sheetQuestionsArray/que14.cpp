8ce670a7b73953bc500afe6f75c23371f0c9d73e
paritosh <paritoshpurohit29@gmail.com>
2025-04-25 11:10:11 +0530

    Questions

--- sheetQuestionsArray/que14.cpp
+++ sheetQuestionsArray/que14.cpp
@@ -1,11 +1,11 @@
-//Write a program to initialize a vector with {1, 4, 9, 16, 25}, 
+//Write a program to initialize a Array with {1, 4, 9, 16, 25}, 
 //then replace each element with its square root.
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int array[5]={1,4,9,16,25};
+    int arr[5]={1,4,9,16,25};
     for(int i=0;i<5;i++){
-        
+        arr[i]=
     }
     return 0;
 }
